8c2ad27dce1429d54500ae3bcb801dbca48e504f
Mauro Garcia Coto <maurogarciacoto@gmail.com>
2023-11-16 01:50:05 -0300

    input

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	cout << "Write something:\n";
+	string something;
+	cin >> something;
+	cout << "hehe, you said " << something << "!\n";
+	return 0;
+}
